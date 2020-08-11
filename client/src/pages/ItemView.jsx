b82a6e1430be239ac6e02e7ef405ee65f588d125
ZackFiner <zackary.finer@yahoo.com>
2020-08-10 22:31:29 -0700

    Image gallery for items

--- client/src/pages/ItemView.jsx
+++ client/src/pages/ItemView.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-
+import ImageGallery from 'react-image-gallery';
 import {ReviewView} from '../components'
 
 import api from '../api';
@@ -25,34 +25,40 @@ class ItemView extends Component {
             description: '',
             reviews: [],
             thumbnail_img: '',
-
+            gallery_images: [],
             isLoading: false,
         };
     }
 
     componentDidMount = async () => {
         this.setState({isLoading: true});
+        let gallery_images = [];
+        
+        if (item.data.data.gallery_images) {
+            gallery_images = item.data.data.gallery_images.map( img => ({original: img.path, thumbnail: img.path}) );
+        }
 
         api.getItemById(this.state.id).then( item => {
-           
             this.setState({
                 name: item.data.data.name,
                 description: item.data.data.description,
                 reviews: item.data.data.reviews,
                 thumbnail_img: item.data.data.thumbnail_img,
+                gallery_images: gallery_images,
                 isLoading: false,
             })
         })
     }
 
     render() {
-        const {name, description, reviews, thumbnail_img} = this.state;
+        const {name, description, reviews, thumbnail_img, gallery_images} = this.state;
         const reviewsSection = reviews ? reviews.map( reviewId => {
             return <ReviewView id={reviewId}/>
         }) : [];
         return (
             <Wrapper>
                 <Title>{name}</Title>
+                <ImageGallery items={gallery_images} />
                 <DescriptionArea>
                     <DescriptionText>{description}</DescriptionText>
                 </DescriptionArea>

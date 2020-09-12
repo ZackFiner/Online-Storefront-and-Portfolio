69dc44410b891315ce72564e19316597cf068bb3
ZackFiner <zackary.finer@yahoo.com>
2020-09-12 15:42:21 -0700

    RBAC work

--- models/role-model.js
+++ models/role-model.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
+const RolePermissionModel = new Schema({
+    op_perm: {type: String, required: true},
+    obj_perm: {type: [mongoose.Types.ObjectId], required: true},
+},
+{timestamps: true},
+)
+
 const UserRoleModel = new Schema({
     name: {type: String, required: true},
-    permissions: {type: [String], required: true},
+    permissions: {type: [RolePermissionModel], required: true},
 },
 {timestamps: true},
 )

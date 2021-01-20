f96ff0e7797ded18db026942db571b001963a241
ZackFiner <zackary.finer@yahoo.com>
2021-01-20 11:41:57 -0800

    API methods
    i've setup a text index in mongodb aswell as the api methods for accessing and processing search requests

--- models/item-model.js
+++ models/item-model.js
@@ -7,11 +7,27 @@ const StoreItemSchema = new Schema(
         reviews: {type: [mongoose.Types.ObjectId], required: true},
         gallery_images: {type: [mongoose.Types.ObjectId], required: false},
         description: {type: String, required: true},
+        keywords: {type: [String], required: false},
         thumbnail_img: {type: mongoose.Types.ObjectId, required: false},
     },
     {timestamps: true},
 )
 
+// create a compound index for our item schema to allow full-text search
+StoreItemSchema.index(
+    {
+        name:'text',
+        keywords: 'text',
+        description: 'text'
+    },
+    {
+        name: 'default_search_index',
+        weights: {
+            name: 10,
+            keywords: 5,
+            description: 3,
+        }
+    });
 
 const StoreItem = mongoose.model('store_items', StoreItemSchema);
 module.exports = StoreItem;

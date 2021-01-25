c0255690aee782a6551d792a6509028a8adf6648
ZackFiner <zackary.finer@yahoo.com>
2021-01-25 15:48:10 -0800

    login rework
    i've reworked the appearance of the login screen (as well as changed the title of the site to storefront). I plan on reworking all of the input forms before closing this ticket.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,27 +6,49 @@ import {loadAccount} from '../redux/actions/accountAct'
 import {connect} from 'react-redux';
 
 const Wrapper = styled.div.attrs({
+})`
+    padding-top: 5rem;
+`
+const InputWrapper = styled.div.attrs({
     className: 'form-group',
 })`
 `
 
-const TextInputSection = styled.div.attrs({
-    className: 'form-text-input-area'
+const FormWrapper = styled.form.attrs({
 })`
-    display: grid-inline;
-    grid-template-columns: auto auto;
+    background-color: #f0f0f0;
+    max-width: 20rem;
+    max-height: 70rem;
+    padding: 1rem;
+    margin: auto;
+    border-radius: 10px;
+    border-color: #efefef;
+    border-style: solid;
+    border-width: 2px;
+
 `
+
 const Label = styled.label`
     margin: 5px;
 `
 
 const InputText = styled.input.attrs({
     className: 'form-control',
+    placeholder: 'example@email.com',
 })`
-    margin: 5px;
 `
-const Button = styled.button.attrs({
+
+const PasswordInput = styled.input.attrs({
+    className: 'form-control',
+    type: 'password',
+    placeholder: 'Password'
+})`
+`
+
+const SubmitButton = styled.input.attrs({
     className: `btn btn-primary`,
+    type: 'submit',
+    value: 'Login',
 })`
     margin: 15px 15px 15px 15px;
 `
@@ -66,12 +88,19 @@ class LoginPage extends Component {
     render() {
         return (
             <Wrapper>
-                <TextInputSection>
-                    <Label>Email</Label><InputText onChange={this.handleChangeEmailText} />
-                    <Label>Password</Label><InputText onChange={this.handleChangePasswordText} />
-                </TextInputSection>
-                <Button onClick = {this.handleSubmit}>Submit</Button>
-                <CancelButton>Cancel</CancelButton>
+                
+                <FormWrapper onSubmit={this.handleSubmit}>
+                    <h2>Login</h2>
+                    <InputWrapper>
+                        <Label>Email</Label>
+                        <InputText onChange={this.handleChangeEmailText} />
+                    </InputWrapper>
+                    <InputWrapper>
+                        <Label>Password</Label>
+                        <PasswordInput onChange={this.handleChangePasswordText} />
+                    </InputWrapper>
+                    <SubmitButton />
+                </FormWrapper>
             </Wrapper>
         )
     }

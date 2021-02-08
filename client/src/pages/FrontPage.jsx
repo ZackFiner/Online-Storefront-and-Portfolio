bb8a1a4d3b92189a7233fff0348657280b7eb15e
ZackFiner <zackary.finer@yahoo.com>
2021-02-07 19:35:28 -0800

    Update FrontPage.jsx

--- client/src/pages/FrontPage.jsx
+++ client/src/pages/FrontPage.jsx
@@ -70,6 +70,16 @@ const DragNDropHanlder = styled.div.attrs({
     left:0;
     z-index: 250;
 
+    &.expanded ~ div.expander {
+        height:  300px;
+    }
+`
+const Expander = styled.div.attrs({
+    className: 'expander'
+})`
+    width: 100%;
+    height: 0;
+    
 `
 
 const PostHeader = styled.h1.attrs({
@@ -94,6 +104,8 @@ class FrontPage extends Component {
         this.state = {
             posts: [],
         }
+        this.dragedIndex = -1;
+        this.draggedId = null;
     }
 
     componentDidMount = () => {
@@ -107,11 +119,32 @@ class FrontPage extends Component {
     handleDrag = (event) => {
         const id = event.target.id;
         event.target.classList.add("dragging");
-        event.dataTransfer.setData('post_id', id);
+        const index = event.target.getAttribute('name');
+        console.log(index);
+        event.dataTransfer.setData('post_index', index);
+        this.draggedId = id;
+        this.dragedIndex = index;
     }
 
     handleDragOver = (event) => {
         event.preventDefault();
+
+    }
+
+    handleDragEnter = (event) => {
+        const post_index = this.dragedIndex;
+        const id = parseInt(event.target.id.substr(9));
+
+        if (post_index != id) 
+            event.target.classList.add('expanded');
+    }
+
+    handleDragLeave = (event) => {
+        const post_index = this.dragedIndex;
+        const id = parseInt(event.target.id.substr(9));
+
+        if (post_index != id) 
+            event.target.classList.remove('expanded');
     }
 
     handleDragStop = (event) => {
@@ -120,7 +153,9 @@ class FrontPage extends Component {
 
     handleDrop = (event) => {
         event.preventDefault();
-        const post_id = event.dataTransfer.getData('post_id');
+        event.target.classList.remove("dragging");
+        event.target.classList.remove('expanded');
+        const post_id = this.draggedId;
         const id = parseInt(event.target.id.substr(9));
         let {posts} = this.state;
         const source_indx = posts.findIndex(p => p._id==post_id);
@@ -132,6 +167,8 @@ class FrontPage extends Component {
             // save changes to ordering
             this.setState({posts:posts});
         }
+        this.dragedIndex = -1;
+        this.draggedId = null;
         
     }
 
@@ -157,16 +194,17 @@ class FrontPage extends Component {
                     <EditBtn id={p._id}>Edit</EditBtn>,
                     <DeleteButton onClick={delete_post}>Delete</DeleteButton>
                 ];
-                drag_pane = <DragNDropHanlder id={`dropzone_${p.index}`} onDragOver={this.handleDragOver} onDrop={this.handleDrop}></DragNDropHanlder>;
+                drag_pane = <DragNDropHanlder id={`dropzone_${p.index}`} onDragLeave={this.handleDragLeave} onDragEnter={this.handleDragEnter} onDragOver={this.handleDragOver} onDrop={this.handleDrop}></DragNDropHanlder>;
             }
             return (
                 <PostPos id={`postc_${p.index}`} key={p.index}>
-                    <PostWrapper id={p._id} key={p._id} draggable={edit} onDragEnd={this.handleDragStop} onDragStart={this.handleDrag}>
+                    <PostWrapper id={p._id} name={p.index} key={p._id} draggable={edit} onDragEnd={this.handleDragStop} onDragStart={this.handleDrag}>
                         <PostContainer>
+                            {drag_pane}
+                            <Expander></Expander>
                             <PostHeader>{p.header}</PostHeader>
                             <PostInfo>{new Date(p.createdAt).toLocaleString()}</PostInfo>
                             <PostContent dangerouslySetInnerHTML={{__html: p.content}}/>
-                            {drag_pane}
                         </PostContainer>
                     </PostWrapper>
                     {edit_panel}

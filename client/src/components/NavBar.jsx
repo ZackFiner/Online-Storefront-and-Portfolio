57c0a7333b5da8072fa83bcae5d98fc788672ded
ZackFiner <zackary.finer@yahoo.com>
2021-02-15 17:51:10 -0800

    deprecated file removal

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Logo from './Logo';
 import Links from './Links';
 import LoginIndicator from './LoginIndicator';
 

2497ed92f38ec1b41bb61b3443c354863e17e398
ZackFiner <zackary.finer@yahoo.com>
2021-06-05 22:41:17 -0700

    changing API url structure
    Moved the authentication functions into the user management API because they're so closely coupled (both need access to the same Database documents, both are logically related to user account management and use).

--- routes/user-router.js
+++ routes/user-router.js
@@ -6,4 +6,9 @@ const router = express.Router();
 
 router.post('/', UserCtrl.createUser);
 router.get('/', withAuth, UserCtrl.getUserData); // users must log in to access user data
+
+router.post('/authenticate', UserCtrl.authUser);
+router.delete('/authenticate', UserCtrl.userLogOut);
+router.put('/authenticate', withAuth, UserCtrl.refreshUserToken); // user must be logged in to access this
+
 module.exports = router;

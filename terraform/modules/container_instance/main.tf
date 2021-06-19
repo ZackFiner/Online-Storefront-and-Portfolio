6bb62b486bc9403cc34c35765aa63d08fceb0c08
ZackFiner <zackary.finer@yahoo.com>
2021-06-19 13:01:03 -0700

    auto-scaling instead of single instance

--- terraform/modules/container_instance/main.tf
+++ terraform/modules/container_instance/main.tf
@@ -1,30 +1,75 @@
-data "aws_ami" "ecs_linux_ami" {
-    most_recent = true // get the most recent ami if multiple are returned
-    owners = ["amazon"]
+// ---------------------- IAM profile creation ---------------------- 
 
-    filter { // select amis with names starting with amzn2-ami-ecs-*
-        name = "name"
-        values =  ["amzn2-ami-ecs-*"]
+data "aws_iam_policy_document" "ecs_instance" { // ecs instance role document
+    statement {
+        actions     = ["sts:AssumeRole"]
     }
 
-    filter { // select the hardware virtualization images
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-
-    filter { // select images with the EBS root device type
-        name= "root-device-type"
-        values = ["ebs"]
+    principals {
+        type        = "Service"
+        identifiers = ["ec2.amazonaws.com"]
     }
 }
 
-resource "aws_instance" "container_instance" {
-    ami = data.aws_ami.ecs_linux_ami.id
-    instance_type = "t2.micro"
+resource "aws_iam_role" "ecs_instance_role" { // role for ecs instances
+    name                = "ecsInstanceRole"
+    assume_role_policy  = data.aws_iam_policy_document.ecs_instance.json
+}
 
+resource "aws_iam_role_policy_attachement" "ecs_instance" { // policy for ecs instances
+    role        = aws_iam_role.ecs_instance_role.name
+    policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role" // role needed for ecs instances to be managed
+}
+
+resource "aws_iam_instance_profile" "ecs_instance" {
+    name = "ecsInstanceProfile"
+    role = aws_iam_role.ecs_instance_role.name
+}
+
+// ---------------------- Instance Creation ---------------------- 
+
+data "aws_ami" "ecs_linux_ami" {
+    most_recent = true // get the most recent ami if multiple are returned
+    owners      = ["amazon"]
+
+    filter { // select amis with names starting with amzn2-ami-ecs-*
+        name    = "name"
+        values  =  ["amzn2-ami-ecs-*"]
+    }
+
+    filter { // select the hardware virtualization images
+        name    = "virtualization-type"
+        values  = ["hvm"]
+    }
+
+    filter { // select images with the EBS root device type
+        name    = "root-device-type"
+        values  = ["ebs"]
+    }
+}
+
+resource "aws_launch_configuration" "container_instance" {
+    name_prefix                 = "ecs-cluster-instance-"
+    image_id                    = data.aws_ami.ecs_linux_ami.id
+    instance_type               = var.instance_type
+
+    iam_instance_profile        = aws_iam_instance_profile.ecs_instance.name
     associate_public_ip_address = var.associate_public_ip
-    subnet_id = var.subnet_id
-    vpc_security_group_ids = var.security_group_ids
+    security_groups             = var.security_group_ids
+    user_data                   = "#!/bin/bash\necho ECS_CLUSTER=${var.cluster} >> /etc/ecs/ecs.config"
+}
 
-    // TODO:
+resource "aws_autoscaling_group" "container_instances" {
+    name = "${var.cluster}_instances"
+
+    // for now, only allow 1 instance at a time
+    desired_capacity        = 1
+    max_size                = 1
+    min_size                = 1
+    force_delete            = true
+
+    launch_configuration    = aws_launch_configuration.container_instance.name
+    vpc_zone_identifier     = var.subnet_ids
+
+    load_balancers          = var.load_balancer_arns
 }
--- terraform/modules/container_instance/vars.tf
+++ terraform/modules/container_instance/vars.tf
@@ -1,4 +1,8 @@
 variable "vpc_id" { type=string }
 variable "security_group_ids" { type=list(string) }
-variable "subnet_id" { type=string }
+variable "subnet_ids" { type=list(string) }
 variable "associate_public_ip" { type=bool }
+variable "instance_type" { type=string, default="t2.micro" }
+variable "zones" { type=list(string), default=["us-west-2"] }
+variable "cluster" { type=string }
+variable "load_balancer_arns" {type=list(string), description="arns to classic load balancers"}

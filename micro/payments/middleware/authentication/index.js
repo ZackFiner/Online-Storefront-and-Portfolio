1f8ce2e31ca498926d9761ce5a1dfba066de2da0
ZackFiner <zackary.finer@yahoo.com>
2021-07-06 20:00:50 -0700

    header authorization
    i forgot to switch the authentication middleware for api methods and webhooks to use the authorization header instead of cookies for communicating authentication information.

--- micro/payments/middleware/authentication/index.js
+++ micro/payments/middleware/authentication/index.js
@@ -1,6 +1,7 @@
 const withAuthMiddleware = require('./decode-middleware');
+const withHeaderAuthMiddleware = require('./decode-header-middleware');
 const {secret, api_secret, verify_options} = require('../../data/server-config');
 const withAuth = withAuthMiddleware(secret, verify_options)
-const withAPIAuth = withAuthMiddleware(api_secret, verify_options);
+const withAPIAuth = withHeaderAuthMiddleware(api_secret, verify_options);
 
 module.exports = {withAuth, withAPIAuth};

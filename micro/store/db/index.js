05ceaba1f6c853e2e4ebfbf6060b62aec3b31cfa
ZackFiner <zackary.finer@yahoo.com>
2021-07-07 22:48:39 -0700

    switching to s3 storage
    i'm working on refactoring the existing image storage code to use an AWS s3 bucket instead of cloudinary. This way, i can move the post and store microservices into a private subnet (after i've set up a local mongo db database in the vpc).

--- micro/store/db/index.js
+++ micro/store/db/index.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const {CloudinaryStorage} = require('multer-storage-cloudinary');
-const {cloudinary_config, mongodb_connection} = require('../data/server-config');
+const {cloudinary_config, mongodb_connection, s3_connection, aws_config, bucket_name} = require('../data/server-config');
+
+const aws = require("aws-sdk");
+aws.config.update(aws_config);
+
+const S3Storage = require("multer-s3");
+const s3 = new aws.S3(s3_connection);
+
+const { v4: uuidv4 } = require('uuid');
 
 mongoose
     .connect(mongodb_connection, {useNewUrlParser: true,useUnifiedTopology: true,})
@@ -12,7 +20,7 @@ mongoose
 
 mongoose.set('useFindAndModify', false);
 const mongoosedb = mongoose.connection;
-
+/*
 cloudinary.config(cloudinary_config);
 const imageStorage = new CloudinaryStorage({
     cloudinary: cloudinary,
@@ -22,6 +30,19 @@ const imageStorage = new CloudinaryStorage({
     }
     //transformation: [{ width: 1024, height: 1024, crop:"limit" }]
 });
+*/
+
+const imageStorage = S3Storage({
+    s3: s3,
+    bucket: bucket_name,
+    metadata: (req, file, callback) => {
+        callback(null, {fieldName: file.fieldname});
+    },
+    key: (req, file, callback) => {
+        callback(null, uuidv4());
+    }
+});
+
 const imageParser = multer({ storage: imageStorage });
 
 module.exports = {mongoosedb, imageParser, imageStorage};
